c539fd19407230e41fc2cb9fdcb2433d749cc353
sabbapxakadze <xdxsabuka@gmail.com>
2024-10-25 12:25:44 +0400

    Finished Practice2

---
+++ Day03 - If-Else, Switch, Type Casting/Practice2/Practice2/Practice2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+Console.WriteLine("Enter your first number!");
+int a = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter your second number!");
+int b = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter your third number!");
+int c = int.Parse(Console.ReadLine());
+
+
+if (a + b > c && a + c > b && b + c > a)
+    Console.WriteLine("This should be a triangle!");
+else Console.WriteLine("This can not be a triangle");
