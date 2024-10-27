df602b151e01bbceb6b6c1d2573046d0a27b2160
sabbapxakadze <xdxsabuka@gmail.com>
2024-10-28 02:34:16 +0400

    Finished practice3

---
+++ Day04 - Loops/Practice3/Practice3/Practice3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your number");
+
+int x = int.Parse(Console.ReadLine());
+while (x < 0)
+{
+    Console.WriteLine("Enter nonnegative number:");
+    x = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i <= x; i++)
+{
+    Console.WriteLine($"{i} cubed is {i*i*i}");
+}
