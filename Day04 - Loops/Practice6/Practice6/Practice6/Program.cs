0cf85462d5df6af8760f3a5b4486612dbf4bd054
sabbapxakadze <xdxsabuka@gmail.com>
2024-10-28 02:50:14 +0400

    Finished Practice6

---
+++ Day04 - Loops/Practice6/Practice6/Practice6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter a positive number: ");
+int x = int.Parse(Console.ReadLine());
+
+while (x <= 0)
+{
+    Console.Write("Enter a valid number: ");
+    x = int.Parse(Console.ReadLine());
+}
+
+Console.Write($"The divisors of {x} are: ");
+
+for (int i = 1; i <= x; i++)
+{
+    if (x % i == 0)
+        if (x == i) Console.Write(i);
+        else Console.Write(i + ", ");
+}
