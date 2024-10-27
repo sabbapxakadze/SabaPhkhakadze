21e06c3994c6afc49bde0a2572e483b1e9585348
sabbapxakadze <xdxsabuka@gmail.com>
2024-10-28 03:12:29 +0400

    Finished!

---
+++ Day04 - Loops/Practice8/Practice8/Practice8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter a number: ");
+int x = int.Parse(Console.ReadLine());
+
+while (x < 0)
+{
+    Console.Write("Enter a valid number: ");
+    x = int.Parse(Console.ReadLine());
+}
+
+string r = "";
+int old = x;
+
+while (x != 0)
+{
+    if (x % 2 == 0) r = 0 + r;
+    else r = 1 + r;
+    x /= 2;
+}
+
+Console.WriteLine($"Your number {old} in binary is {r}");
