33644565d2091bdf7e773dc69f6040fdac8d3782
sabbapxakadze <xdxsabuka@gmail.com>
2024-10-31 15:37:48 +0400

    Finished practice8

---
+++ Day06 - Methods/Practice8/Practice8/Practice8/Program.cs
@@ -0,0 +1,35 @@
+﻿static int EnterAPositiveNumber()
+{
+    Console.Write("Enter a positive number: ");
+    int x = int.Parse(Console.ReadLine());
+    while (x <= 0)
+    {
+        Console.Write("Enter a valid positive number: ");
+        x = int.Parse(Console.ReadLine());
+    }
+    return x;
+}
+
+static void PrintTheSumOfDegree(int x)
+{
+    int old = x;
+    string res = "";
+    int deg = 0;
+    
+    while (x != 0)
+    {
+        int rem = x % 10;
+
+        if (x / 10 == 0)
+            res = $"{rem} * 10^{deg} " + res;
+        else 
+            res = $"+ {rem} * 10^{deg} " + res;
+
+        x /= 10;
+        deg = deg + 1;
+    }
+    res = $"{old} = {res}";
+    Console.WriteLine(res);
+}
+
+PrintTheSumOfDegree(EnterAPositiveNumber());
