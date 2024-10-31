2ec9378fe03013d0fdb016fe52170f3be173f458
sabbapxakadze <xdxsabuka@gmail.com>
2024-10-31 18:22:25 +0400

    Finished Practice9

---
+++ Day06 - Methods/Practice9/Practice9/Practice9/Program.cs
@@ -0,0 +1,14 @@
+﻿static void Multiplies(params int[] p)
+{
+
+    int res = 1;
+
+    foreach (int i in p)
+    {
+        res = res * i;
+    }
+
+    Console.WriteLine($"The product of array elements are: {res}");
+}
+
+Multiplies(1,3,4,15,13,23,98);
