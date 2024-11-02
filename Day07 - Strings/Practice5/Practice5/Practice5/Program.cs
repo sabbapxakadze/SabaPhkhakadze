e42408aad7572cb92e79bd8b4cbe68926a17c2d2
sabbapxakadze <xdxsabuka@gmail.com>
2024-11-02 21:35:32 +0400

    Finished Practice5

---
+++ Day07 - Strings/Practice5/Practice5/Practice5/Program.cs
@@ -0,0 +1,28 @@
+﻿static int CountLetters(string s)
+{
+    int cnt = 0;
+    foreach (char c in s)
+    {
+        if (char.IsLetter(c)) cnt++;
+    }
+    return cnt;
+}
+static int CountDigits(string s)
+{
+    int cnt = 0;
+    foreach (char c in s)
+    {
+        if (char.IsDigit(c)) cnt++;
+    }
+    return cnt;
+}       
+
+static void PrintTheResults(string s)
+{
+    int letters = CountLetters(s);
+    int digits = CountDigits(s);
+
+    Console.WriteLine($"\"{s}\": -> Letters: {letters}, Numbers: {digits}, Others: {s.Length - letters - digits}");
+}
+
+PrintTheResults(Console.ReadLine());
