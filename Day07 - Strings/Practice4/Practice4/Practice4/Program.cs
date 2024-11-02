4d74d16df30e90197b9d33cc2aca76f30a155dce
sabbapxakadze <xdxsabuka@gmail.com>
2024-11-02 21:24:00 +0400

    Finished Practice4

---
+++ Day07 - Strings/Practice4/Practice4/Practice4/Program.cs
@@ -0,0 +1,22 @@
+﻿static int CountWords(string x)
+{
+    int cnt = 0;
+    string a = x.Trim();
+
+    string[] arr = a.Split(' ');
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (!String.IsNullOrEmpty(arr[i])) cnt++;
+    }
+    return cnt;
+}
+
+static void PrintCount(string x)
+{
+    int cnt = CountWords(x);
+
+    Console.WriteLine(cnt);
+}
+
+PrintCount(Console.ReadLine());
