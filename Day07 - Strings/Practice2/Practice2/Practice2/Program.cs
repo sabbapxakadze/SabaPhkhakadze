46d93c283406c27f6897a898d4f1ef620257993f
sabbapxakadze <xdxsabuka@gmail.com>
2024-11-02 18:45:22 +0400

    Finished Practice2

---
+++ Day07 - Strings/Practice2/Practice2/Practice2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+static void ReverseString()
+{
+    string x = Console.ReadLine();
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (int i = x.Length - 1; i >= 0; i--)
+    { 
+        stringBuilder.Append(x[i]);
+    }
+
+    string result = stringBuilder.ToString();
+    Console.WriteLine(result);
+}
+
+ReverseString();
