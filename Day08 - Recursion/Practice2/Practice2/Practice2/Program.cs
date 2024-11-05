a2de965eba47a0825546ece8b40a555dc845d464
sabbapxakadze <xdxsabuka@gmail.com>
2024-11-05 15:57:56 +0400

    Finished Practice2

---
+++ Day08 - Recursion/Practice2/Practice2/Practice2/Program.cs
@@ -0,0 +1,8 @@
+﻿void RecursiveSum(int x, int acc = 0) // this is tail-recursive way and by default acc = 0
+{
+    if (x == 1) Console.WriteLine(1 + acc);
+
+    else RecursiveSum(x - 1, acc + x);
+}
+
+RecursiveSum(int.Parse(Console.ReadLine()));
