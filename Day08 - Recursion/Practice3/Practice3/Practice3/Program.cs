facf34e6ed228087b50b3d6d1483622eaefd5832
sabbapxakadze <xdxsabuka@gmail.com>
2024-11-05 16:04:33 +0400

    Finished Practice3

--- Day08 - Recursion/Practice3/Practice3/Practice3/Program.cs
+++ Day08 - Recursion/Practice3/Practice3/Practice3/Program.cs
@@ -2,7 +2,7 @@
 {
     if (x < 0) RecursiveNumberOfLength(-x, acc);
     
-    else if (x>10) RecursiveNumberOfLength(x / 10, acc + 1);
+    else if (x>=10) RecursiveNumberOfLength(x / 10, acc + 1);
 
     else Console.WriteLine(acc + 1);
 }
