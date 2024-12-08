d0e38fc861a2b249c9600061fa1b97cd16504b6d
sabbapxakadze <xdxsabuka@gmail.com>
2024-12-08 17:56:42 +0400

    Finished!

---
+++ Day18 - Reflection/ReflectionPractice/ReflectionPractice/ReflectionPractice/Calculator.cs
@@ -0,0 +1,28 @@
+﻿public static class Calculator
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+    public static double Divide(double a, double b)
+    {
+        if (a == 0) throw new DivideByZeroException();
+        return a / b;
+    }
+    public static double Square(double a)
+    {
+        return a * a;
+    }
+    public static double Multiply5x(double a, double b, double c, double d, double e)
+    {
+        return a * b * c * d * e;
+    }
+}
