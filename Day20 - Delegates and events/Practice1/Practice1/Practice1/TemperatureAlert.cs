38bcdad57b1e0e37f2e38b324857d82c6f4412f3
sabbapxakadze <xdxsabuka@gmail.com>
2024-12-10 03:57:04 +0400

    Finished!

---
+++ Day20 - Delegates and events/Practice1/Practice1/Practice1/TemperatureAlert.cs
@@ -0,0 +1,14 @@
+﻿public class TemperatureAlert
+{
+    public void OnProcessCompletedHandler(object sender, int temp)
+    {
+        if (temp > 40 || temp < 0)
+        {
+            Console.WriteLine("Alert! Critical temperature detected: " + temp);
+        }
+        else
+        {
+            Console.WriteLine("Temperature is okay: " + temp);
+        }
+    }
+}
---
+++ Day20 - Delegates and events/Practice1/Practice1/Practice1/TemperatureMonitor.cs
@@ -0,0 +1,14 @@
+﻿public class TemperatureMonitor
+{
+    public event EventHandler<int> TemperatureChanged;
+
+    public void CheckTemperature(int temperature)
+    {
+        OnTemperatureChanged(temperature);
+    }
+
+    protected virtual void OnTemperatureChanged(int temperature)
+    {
+        TemperatureChanged?.Invoke(this, temperature);
+    }
+}
