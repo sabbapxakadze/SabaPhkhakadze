fbd45220315125696c917b1cf7763fc58e520f5f
sabbapxakadze <xdxsabuka@gmail.com>
2024-12-13 01:40:54 +0400

    Finished!

---
+++ Day22 - LINQ 2/LINQ/LINQ/LINQ/Customer.cs
@@ -0,0 +1,10 @@
+﻿public class Customer
+{
+    public int CustomerID { get; set; }
+    public string CustomerName { get; set; }
+    public Customer(int customerID, string name)
+    {
+        CustomerID = customerID;
+        CustomerName = name;
+    }
+}
---
+++ Day22 - LINQ 2/LINQ/LINQ/LINQ/Order.cs
@@ -0,0 +1,16 @@
+﻿public class Order
+{
+    public int OrderID { get; set; }
+    public long Date { get; set; }
+    public string Product { get; set; }
+    public decimal Price { get; set; }
+    public int CustomerID { get; set; }
+    public Order(int orderID, long date, string product, decimal price, int customerID)
+    {
+        OrderID = orderID;
+        Date = date;
+        Product = product;
+        Price = price;
+        CustomerID = customerID;
+    }
+}
