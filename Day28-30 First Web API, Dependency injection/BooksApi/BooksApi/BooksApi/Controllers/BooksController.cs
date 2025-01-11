761d26070f9000dcc6e11031c0368bfde57727b2
sabbapxakadze <xdxsabuka@gmail.com>
2025-01-11 16:51:10 +0400

    Finished

---
+++ Day28-30 First Web API, Dependency injection/BooksApi/BooksApi/BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using BooksLibrary.Abstract;
+using BooksLibrary.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBook(int id)
+        {
+            var book = _bookService.GetBook(id);
+            if (book == null)
+            {
+                return NotFound(book);
+            }
+            return Ok(book);
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var books = _bookService.GetBooks();
+            if (books == null)
+                return NoContent();
+            return Ok(books);
+        }
+    }
+}
